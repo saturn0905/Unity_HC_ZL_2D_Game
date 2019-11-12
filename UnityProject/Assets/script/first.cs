637417a56265955e43006e8a5805961299ea6566
saturn0905 <57396731+saturn0905@users.noreply.github.com>
2019-11-12 21:31:56 +0800

    添加欄位

---
+++ UnityProject/Assets/script/first.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public class first : MonoBehaviour
+{
+    public int level;//整數
+    public float speed;//浮點數
+    public string prop;//字串
+    public bool mission;//布林值
+
+
+    
